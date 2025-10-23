58b64e087ba6badb43a9b8599df8f3009fe6a3fb
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-10-23 23:32:42 +0300

    Updates on test widget

--- ble_smart_device_scanner/test/widget_test.dart
+++ ble_smart_device_scanner/test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:ble_scanner_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ble_smart_device_scanner/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
