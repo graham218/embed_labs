0996d616d03add2fc4374a09bc6132af1595acdf
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-10-24 00:07:07 +0300

    Debigging

--- ble_smart_device_scanner/android/app/build.gradle.kts
+++ ble_smart_device_scanner/android/app/build.gradle.kts
@@ -8,7 +8,9 @@ plugins {
 android {
     namespace = "com.embedlabs.ble_smart_device_scanner"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    //Fixed NDK version (was flutter.ndkVersion before)
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -22,8 +24,8 @@ android {
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.embedlabs.ble_smart_device_scanner"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+
+        // These use Flutter’s project values — you can adjust if needed.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -33,12 +35,20 @@ android {
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // signingConfig = signingConfigs.release
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
 }
