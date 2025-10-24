274be67d511ce98705f5a060c6bac22b036a3ca2
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-10-24 12:11:42 +0300

    Updating ble scan screen

--- ble_smart_device_scanner/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ble_smart_device_scanner/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import device_info_plus
 import flutter_blue_plus_darwin
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
